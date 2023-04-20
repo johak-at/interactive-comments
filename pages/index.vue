625c012c4b8933a431f86db6d04affcfc051f203
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-04-20 23:22:51 +0200

    minor changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { Icon } from "@iconify/vue";
-import { Camera, CameraResultType } from "@capacitor/camera";
 import { useStore } from "~/store/store";
 import { storeToRefs } from "pinia";
 
@@ -25,29 +23,12 @@ function setName() {
 
 // object for comments
 const comments = ref([{ name: "John", comment: "Hello" }]);
-
-//geolocation via VueUse:
-const { coords, locatedAt, error, resume, pause } = useGeolocation();
-
-// taking a picture and displaying it:
-const imageUrl = ref(null);
-async function takePic() {
-  const image = await Camera.getPhoto({
-    quality: 90,
-    allowEditing: true,
-    resultType: CameraResultType.Uri,
-  });
-  imageUrl.value = image.webPath;
-}
-
-definePageMeta({
-  alias: "/home",
-});
 </script>
 
 <template>
   <div class="">
     <h1 class="text-black">{{ comments[0].comment }}</h1>
+    <div text-black>{{ data }}</div>
   </div>
 </template>
 
